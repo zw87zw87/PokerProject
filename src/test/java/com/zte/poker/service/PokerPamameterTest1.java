d1c6307a3f6579623ac5e8b268090bd8a12d499b
ZTE <zw87zw87@163.com>
2020-01-14 23:44:33 +0800

    modify UT

--- src/test/java/com/zte/poker/service/PokerPamameterTest1.java
+++ src/test/java/com/zte/poker/service/PokerPamameterTest1.java
@@ -8,11 +8,11 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import static org.junit.Assert.assertEquals;
 
 public class PokerPamameterTest1 {
+    private PokerHands hands = new PokerHandsImpl();
 
     @ParameterizedTest(name = "{index}: black={0}, white={1}, expected={2}")
     @CsvFileSource(resources = {"/poker_result.csv"})
-    public void test(String black, String white, GameResult result) throws ParameterException {
-        PokerHands hands = new PokerHandsImpl();
+    void test(String black, String white, GameResult result) throws ParameterException {
         assertEquals(result, hands.playGame(black, white));
     }
 }
